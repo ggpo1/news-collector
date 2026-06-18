21d6b07e9dafed2c26be8bcfbcbd132ef7e686cb
vl.arkhangelskii <vl.arkhangelskii@megapolis-it.ru>
2026-06-18 15:50:23 +0300

    feat(front): change port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5181,
+    port: 5190,
     proxy: {
       '/api': 'http://localhost:5182',
       '/health': 'http://localhost:5182',
